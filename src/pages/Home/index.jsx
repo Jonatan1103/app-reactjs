ab9e99dfff82aff3fd4ffc006c3d80ce1a7beb68
Jonatan Timoteo <jonatan_ts@hotmail.com>
2022-04-19 11:37:39 -0300

    useState

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,24 @@
 import './style.css'
 
 import { Card } from '../../components/Card';
+import { useState } from 'react';
 
 export function Home() {
+  const [studentName, setStudentName] = useState('Amanda')
+
   return (
 
     <div className='container'>
-      <h1>Lista de presença</h1>
-      <input type="text" placeholder="Digite o nome"/>
+      <h1>Name: {studentName}</h1>
+      <input
+        type="text"
+        placeholder="Digite o nome"
+        onChange={event => setStudentName(event.target.value)}
+      />
       <button type="button">Adicionar</button>
 
-      <Card />
-      <Card />
-      <Card />
+      <Card name="Jonatan" time="10:55:25"/>
+      <Card name="Andressa" time="10:25:25"/>
     </div>
 
   )
